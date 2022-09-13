99f473a4f5f08092161597b2100a363723bb4a8b
Lutilaci <lutilaci@gmail.com>
2022-09-13 10:49:58 +0200

    add build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+export BASE_URL=https://jira-auto.codecool.metastage.net
+export PASSWORD=CCAutoTest19.
+export USER_NAME=Automation23
+export BROWSER=CHROME
+mvn test
+unset BASE_URL=https://jira-auto.codecool.metastage.net
+unset PASSWORD=CCAutoTest19.
+unset USER_NAME=Automation23
+unset BROWSER=CHROME
+
